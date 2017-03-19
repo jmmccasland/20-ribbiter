7e0f44a7f3a5bb08a047ab71a7b8b842f6c40670
John Michael McCasland <jmmccasland@gmail.com>
2017-03-19 12:55:41 -0500

    Application Route

---
+++ app/routes/application.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div class="app">
+    <header class="section section--header">
+        <div class="top-nav">
+          <div class="top-nav__left">
+            <h1 class="logo">Ribbeter</h1>
+          </div>
+          <div class="top-nav__right">
+            <a href="" class="top-nav__right__item">Ribbets</a>
+            <a href="" class="top-nav__right__item">Frogs</a>
+            <a href="" class="top-nav__right__item">Login</a>
+          </div>
+        </div>
+    </header>
+
+    <router-view>
+
+
+
+    </router-view>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
---
+++ app/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import Vue from 'vue';
+import Revue from 'Revue';
+
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const initialState = {  };
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+
+export default new Revue(Vue, store);
