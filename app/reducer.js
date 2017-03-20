4de4eb9bff6f744c709d1115f383e2634ea138b0
John Michael McCasland <jmmccasland@gmail.com>
2017-03-20 10:47:57 -0500

    Setup reducer for posts

--- app/reducer.js
+++ app/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userResource from './resources/user';
+import postResource from './resources/post';
 
 export default combineReducers({
   users: userResource.reducer,
+  post: postResource.reducer,
 });
--- app/resources/user.js
+++ app/resources/user.js
@@ -3,16 +3,3 @@ import createResource from 'redux-thunk-rest';
 export default createResource('user', {
   url: 'http://ribbiterapi.herokuapp.com/users',
 });
-
-
-//  do i need this ?
-// Using the reducers
-// const reducer = combineReducers({
-//   users: userResource.reducer,
-// });
-//
-// // Using the action creator
-// // const findAll = bookResource.actionCreators.findAll
-// const { actionCreators: { findAll } } = userResource;
-//
-// store.dispatch(findAll());
